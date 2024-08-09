710f77189e52078143f2d72beefcd25f76df37f3
s21sd <srivastavasunny359@gmail.com>
2024-08-09 21:37:42 +0530

    some more commits

--- src/app/utils/Firebase/firebase.js
+++ src/app/utils/Firebase/firebase.js
@@ -2,14 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDy_7ChkVKdsE74pK4oDrd3KmvZKOsmsh8",
-    authDomain: "tailfusion-4ae92.firebaseapp.com",
-    projectId: "tailfusion-4ae92",
-    storageBucket: "tailfusion-4ae92.appspot.com",
-    messagingSenderId: "1001660398145",
-    appId: "1:1001660398145:web:e4cc6b4eed98ba4b8764c0"
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
