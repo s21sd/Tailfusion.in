6edb0796d075c0eb0c69a01c18862844266e33d7
s21sd <srivastavasunny359@gmail.com>
2024-08-12 18:53:15 +0530

    somre more

--- src/app/components/Landing.tsx
+++ src/app/components/Landing.tsx
@@ -1,7 +1,9 @@
 "use client";
 import React, { Suspense, lazy } from 'react';
 import { TypewriterEffectSmoothDemo } from '@/components/Blocks/Typeeffect';
-import finalPng from '../../assets/Final.png';
+import finalPng from '../../assets/phonef.png';
+import macPng from '../../assets/macf.png';
+// import macvideo from '../../../public/assests/videos/'
 import Image from 'next/image';
 import { MessageBox } from '@/components/Blocks/MessageBox';
 import { AnimatedShinyTextDemo } from '@/components/Blocks/Shinytextdemo';
@@ -9,7 +11,6 @@ import { TextRevealDemo } from '@/components/Blocks/TextReveal';
 import { BentoGridDemo } from '@/components/Blocks/BantoGrid';
 import { BoxRevealDemo } from '@/components/Blocks/Reveatext';
 import { TextGenerateEffectDemo } from '@/components/Blocks/TextGenerationDemo';
-
 const GlobeDemoComp = lazy(() => delayForDemo(import('@/components/Blocks/GlobeDemo')));
 async function delayForDemo(promise: any) {
     await new Promise(resolve => {
@@ -22,22 +23,38 @@ const Landing = () => {
         <div className="container px-5 py-12 sm:py-24 mx-auto relative">
             <AnimatedShinyTextDemo />
 
-            <div className='fixed bottom-5 right-5 z-10'>
+            <div className='fixed right-6 bottom-5 sm:right-5 z-10'>
                 <MessageBox />
             </div>
 
-            <div className="flex flex-col justify-center mx-auto text-center w-full mb-5 mt-5">
+            <div className="flex flex-col justify-center mx-auto text-center w-full mb-5 ">
                 <TypewriterEffectSmoothDemo />
                 <div className='flex justify-center items-center'>
                     <BoxRevealDemo />
                 </div>
 
             </div>
-
-            <div className='flex justify-center items-center mt-5 sm:mt-20 p-10 rounded-xl'>
-                <Image className='rounded-xl w-[100%] sm:w-[80%]' sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                    alt='phoneImg' src={finalPng} />
+            <div className='flex flex-col sm:flex-row justify-center items-center mt-5 sm:mt-20 p-10 space-x-4'>
+                <video
+                    className='rounded-xl w-full sm:w-[50%]'
+                    loop
+                    muted
+                    autoPlay
+                    src='/assests/videos/mac.mp4'
+                >
+                    Your browser does not support the video tag.
+                </video>
+                <video
+                    className='rounded-xl w-full sm:w-[50%]'
+                    loop
+                    muted
+                    autoPlay
+                    src='/assests/videos/phone.mp4'
+                >
+                    Your browser does not support the video tag.
+                </video>
             </div>
+
             <div className='grid sm:flex'>
                 <TextRevealDemo />
                 <BentoGridDemo />
@@ -53,3 +70,9 @@ const Landing = () => {
 };
 
 export default Landing;
+
+
+{/* <Image className='rounded-xl w-[100%] sm:w-[80%]' sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    alt='phoneImg' src={finalPng} />
+                <Image className='rounded-xl w-[100%] sm:w-[80%]' sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    alt='phoneImg' src={macPng} /> */}
--- src/components/Blocks/Reveatext.tsx
+++ src/components/Blocks/Reveatext.tsx
@@ -28,21 +28,20 @@ export function BoxRevealDemo() {
             </BoxReveal>
 
             <BoxReveal boxColor={getTheBoxColor()} duration={0.7}>
-                <div className="mt-[1.5rem] w-full sm:min-w-[20rem] text-xs sm:text-xl">
+                <div className="mt-[1.5rem] w-full sm:min-w-[20rem] text-xs sm:text-xl ">
                     <p className="text-white dark:text-black">
                         16+ free and open-source animated components built with
                         <span className="font-semibold text-[#fff2c1] dark:text-[#9e4ceb]"> React</span>,
-                        <span className="font-semibold text-[#fff2c1] dark:text-[#9e4ceb]"> Typescript</span>,
-                        <span className="font-semibold text-[#fff2c1] dark:text-[#9e4ceb]"> Tailwind CSS</span>,
-                        and
+                        {/* <span className="font-semibold text-[#fff2c1] dark:text-[#9e4ceb]"> Typescript</span>, */}
+                        <span className="font-semibold text-[#fff2c1] dark:text-[#9e4ceb]"> Tailwind</span>
                         . <br />
-                        Copy paste the most trending components and use them in your websites without having to worry about styling.. <br />
+                        Easily copy and paste the most trending components with provided React, Tailwind, HTML and CSS code, integrate them into your projects without worrying about styling.. <br />
                     </p>
                 </div>
             </BoxReveal>
             <div className="grid sm:flex  items-center justify-center gap-4 mt-4 text-sm">
                 <BoxReveal boxColor={getTheBoxColor()} duration={0.5}>
-                    <div onClick={() => router.push('components/docs')} className="bg-[#fff2c1] p-6 hover:bg-gray-50 rounded-md w-[230px] cursor-pointer font-semibold text-black flex justify-center items-center mx-auto dark:hover:bg-[#c98fff] dark:bg-[#9e4ceb] dark:text-white">Browse Components</div>
+                    <div onClick={() => router.push('components/blocks/cards')} className="bg-[#fff2c1] p-6 hover:bg-gray-50 rounded-md w-[230px] cursor-pointer font-semibold text-black flex justify-center items-center mx-auto dark:hover:bg-[#c98fff] dark:bg-[#9e4ceb] dark:text-white">Browse Components</div>
                 </BoxReveal>
                 <BoxReveal boxColor={getTheBoxColor()} duration={0.5}>
                     <div onClick={handleClick} className="border rounded-md p-4 w-[230px] font-semibold cursor-pointer text-white border-[#fff2c1] dark:border-[#9e4ceb] dark:text-black flex justify-center items-center mx-auto gap-4"><FaGithub size={32} />
